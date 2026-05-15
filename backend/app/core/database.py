4574f814ce0b71f53a98dbf352d7fa3531c88b2c
wonsangcju <wonsang200002@gmail.com>
2026-05-16 05:47:38 +0900

    db postgresql로 변경
    username
    password
    compiler
    부분 임시로 해둔거라 이름 바꿔야함.

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,14 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bpp_project.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql+psycopg2://username:password@localhost:5432/compiler"
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
